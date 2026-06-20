427f77f369bb9682d79fe3e0c61c21f25a9a0c7b
Tambet Väli <119508958+tambetvali@users.noreply.github.com>
2026-06-20 10:54:59 +0000

    The generator I add is right now broken at end, but a lot of files generated at the flow: I will eat, then finish this part.

--- CanvasMode/generator.py
+++ CanvasMode/generator.py
@@ -20,5 +20,111 @@ if not os.path.exists("lanedatabase"):
     os.mkdir("lanedatabase")
 
 with open("lanedatabase/lanes.json", "w") as f:
-    json.dump(data, f, indent=4)  # indent for pretty formatting
+    json.dump(data, f, indent=4)
+
+if not os.path.exists("lanedatabase/twoparts"):
+    os.mkdir("lanedatabase/twoparts")
+
+with open("lanedatabase/twoparts/axes.json", "w") as f:
+    json.dump(data["axes"], f, indent=4)
+
+with open("lanedatabase/twoparts/lanes.json", "w") as f:
+    json.dump(data["R"], f, indent=4)
+
+if not os.path.exists("lanedatabase/fullRs"):
+    os.mkdir("lanedatabase/fullRs")
+
+for Rkey, Rdata in data["R"].items():
+    with open("lanedatabase/fullRs/R" + str(Rkey) + ".json", "w") as f:
+        json.dump(Rdata, f, indent=4)
+
+
+if not os.path.exists("lanedatabase/numbers"):
+    os.mkdir("lanedatabase/numbers")
+
+for Rkey, Rdata in data["R"].items():
+    if not os.path.exists("lanedatabase/numbers/" + str(Rkey)):
+        os.mkdir("lanedatabase/numbers/" + str(Rkey))
+
+    for Nkey, Ndata in data["R"][Rkey].items():
+        with open("lanedatabase/numbers/" + str(Rkey) + "/" + "L" + Nkey + ".json", "w") as f:
+            json.dump(Ndata, f, indent=4)
+
+
+# exit()
+
+# I am in the middle of the following simplification, now going to eat..
+
+def simpnum(key, val):
+    number = {}
+    number["Ten"] = {}
+    number["Ten"]["Signed"] = {}
+    number["Ten"]["Unsigned"] = {}
+    number["Dec"] = {}
+    number["Ten"]["Signed"] = {}
+    number["Ten"]["Unsigned"] = {}
+
+    for Pkey, Pdata in val.items():
+        print(val)
+
+        number["Ten"]["Signed"][Pkey] = {}
+        number["Ten"]["Unsigned"][Pkey] = {}
+        number["Ten"]["Signed"][Pkey] = {}
+        number["Ten"]["Unsigned"][Pkey] = {}
+
+        print("Val: ", val)
+
+        number["Ten"]["Signed"][Pkey]["X"] = val["Ten"]["Signed"]["X"]
+        number["Ten"]["Signed"][Pkey]["Y"] = val["Ten"]["Signed"]["Y"]
+
+        number["Ten"]["UnSigned"][Pkey]["X"] = val["Ten"]["UnSigned"]["X"]
+        number["Ten"]["UnSigned"][Pkey]["Y"] = val["Ten"]["UnSigned"]["Y"]
+
+        number["Dec"]["Signed"][Pkey]["X"] = val["Dec"]["Signed"]["X"]
+        number["Dec"]["Signed"][Pkey]["Y"] = val["Dec"]["Signed"]["Y"]
+
+        number["Dec"]["UnSigned"][Pkey]["X"] = val["Dec"]["UnSigned"]["X"]
+        number["Dec"]["UnSigned"][Pkey]["Y"] = val["Dec"]["UnSigned"]["Y"]
+
+        number["Dec"]["Signed"] = {}
+        number["Dec"]["Unsigned"] = {}
+
+    return key, number
+
+if not os.path.exists("lanedatabase/simpRs/"):
+    os.mkdir("lanedatabase/simpRs/")
+
+if not os.path.exists("lanedatabase/numbers.simp/"):
+    os.mkdir("lanedatabase/numbers.simp/")
+
+if not os.path.exists("lanedatabase/numbers.simp/" + str(Rkey)):
+    os.mkdir("lanedatabase/numbers.simp/" + str(Rkey))
+
+for Rkey, Rdata in data["R"].items():
+    if not os.path.exists("lanedatabase/numbers.simp/" + str(Rkey)):
+        os.mkdir("lanedatabase/numbers.simp/" + str(Rkey))
+
+    simpR = {}
+
+    for Nkey, Ndata in data["R"][Rkey].items():
+        _, SNdata = simpnum(Nkey, Ndata)
+
+        with open("lanedatabase/numbers/" + str(Rkey) + "/" + "L" + Nkey + ".json", "w") as f:
+            json.dump(SNdata, f, indent=4)
+        
+        simpR[NKey] = SNdata
+
+    if not os.path.exists("lanedatabase/simpRs/" + str(Rkey)):
+        os.mkdir("lanedatabase/simpRs/" + str(Rkey))
+
+    with open("lanedatabase/simpRs/R" + str(Rkey) + ".json", "w") as f:
+        json.dump(Rdata, f, indent=4)
+
+
+#if not os.path.exists("lanedatabase/simpRs"):
+#    os.mkdir("lanedatabase/simpRs")
+
+#for Rkey, Rdata in data["R"].items():
+#    with open("lanedatabase/fullRs/R" + str(Rkey) + ".json", "w") as f:
+#        json.dump(Rdata, f, indent=4)
 
